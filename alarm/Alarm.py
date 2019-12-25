e73c8f12cc97714b71df1d5d65a9f259e77a0cf5
Vincent Elias <vincent.elias@hotmail.com>
2019-12-25 11:29:27 +0000

    added code

--- alarm/Alarm.py
+++ alarm/Alarm.py
@@ -8,6 +8,7 @@ from time import sleep
 import asyncio
 import RPi.GPIO as GPIO
 import threading
+import json
 from azure.iot.device.aio import IoTHubDeviceClient
 
 #for DHT11 temperature sensor
@@ -37,15 +38,15 @@ class Alarm:
 
 	async def start(self):
 		await self.connect_azure()
-		self.start_alarm()
+		await self.start_alarm()
 
 
-	def start_alarm(self):
+	async def start_alarm(self):
 		while True:
-			temperature=self.get_temperature()
+			humidity,temperature=self.get_dht11_data()
 			mq3_data=self.get_mq3_data()
 			mq9_data=self.get_mq9_data()
-			self.save_measurements(temperature,mq3_data,mq9_data)
+			await self.save_measurements(temperature,humidity,mq3_data,mq9_data)
 
 
 			if((mq3_data>Alarm.gas_threshold or mq9_data>Alarm.smoke_threshold or temperature>Alarm.temperature_threshold) and not self.fire_detected):
@@ -55,39 +56,53 @@ class Alarm:
 				buzzer_thread.start()
 
 
-	def save_measurements(self,temperature,mq3_data,mq9_data):
+	async def save_measurements(self,temperature,humidity,mq3_data,mq9_data):
 		print("saving measurements")
 
-		gas_percentage=self.convert_percentage(mq3_data,230,1000)
+		gas_percentage=self.convert_percentage(mq3_data,350,1000)
 		co_percentage=self.convert_percentage(mq3_data,990,5000)
+		print("gas: "+str(mq3_data))
+		print("Temperature: "+str(temperature)+"	Humidity: "+str(humidity)+"	gas percentage: "+str(gas_percentage)+"%	o percentage: "+str(co_percentage)+"%")
 		
-			# data = {
-			# 	'device_id': 1,
-			# 	'Event': "measurement",
-			# 	'temperature': float(temp),
+		data = {
+			'device_id': 1,
+			'Event': "measurement",
+			'temperature': float(temperature),
+			'gas_percentage':gas_percentage,
+			'co_percentage':co_percentage,
+			'is_fire':1 if self.fire_detected else 0,
+			'humidity':float(humidity)
+		}
 
-			# }
+		json_body = json.dumps(data)
+		print("Sending message: ", json_body)
+		await self.device_client.send_message(json_body)
+		twin = await self.device_client.get_twin()
+		self.handle_twin(twin)
 
-			# json_body = json.dumps(data)
-			# print("Sending message: ", json_body)
-			# await device_client.send_message(json_body)
-			# twin = await device_client.get_twin()
-			# handle_twin(twin)
+		await self.device_client.disconnect()
 
-		#await device_client.disconnect()
+
+	def handle_twin(self,twin):
+		print("Twin received", twin)
+		if ('desired' in twin):
+			desired = twin['desired']
+			if ('is_fire' in desired):
+				if desired['is_fire']==1:
+					buzzer_thread = threading.Thread(target=self.buzzer.start)
+					buzzer_thread.start()
+
 
 	def convert_percentage(self,number,min,max):
 		percentage = round(((number-min)/(max-min))*100,2)
 		if percentage > 100:
-			return 100
+			percentage=100
 		elif percentage<0:
-			return 0
-		else:
-			return percentage
+			percentage=0
+		return float(percentage)
 
-	def get_temperature(self):
-		humidity,temperature = Adafruit_DHT.read_retry(11, 4)
-		return temperature
+	def get_dht11_data(self):
+		return Adafruit_DHT.read_retry(11, 4)
 
 	def get_mq3_data(self):
 		return self.adc.read(0)
--- alarm/Buzzer.py
+++ alarm/Buzzer.py
@@ -11,20 +11,12 @@ class Buzzer:
 		self.p=GPIO.PWM(23,5000)
 
 	def start(self):
-		print("starting buzzer..")
 		self.is_on=True
 		while self.is_on:
 			self.turn_on_for(10,1000,.08)
 			self.turn_on_for(10,2000,.08)
 			self.turn_off_for(.01)
-			
-	def pause(self,seconds):
-		self.turn_off()
-		sleep(seconds)
 
-	def stop(self):
-		self.is_on=False
-		GPIO.cleanup()
 
 	def turn_on_for(self,duty_cycle,frequency,duration):
 		self.p.ChangeFrequency(frequency)
